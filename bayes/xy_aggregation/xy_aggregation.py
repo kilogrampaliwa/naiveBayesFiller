04e7b287c86ced3c20d0ade082652df8209c65cc
Filip Ciąder <filip.ciader@gmail.com>
2024-04-05 21:29:41 +0200

    xy_aggregation repaired

--- bayes/xy_aggregation/xy_aggregation.py
+++ bayes/xy_aggregation/xy_aggregation.py
@@ -5,7 +5,7 @@ from typing import Union
 class table_aggregation:
     "Creates table and returns divided on X_train, Y_train, X_test."
 
-    def __init__(self, table: list[list], Y_train_col: Union[int, bool] = False, X_test_row: Union[int, bool] = False):
+    def __init__(self, table: list[list], x: Union[int, bool] = False, y: Union[int, bool] = False):
         "Creates table and returns if directed X_train, Y_train, X_test."
 
         # main table
@@ -17,15 +17,15 @@ class table_aggregation:
         self.__X_test   = []
 
         # if data for division already given
-        if Y_train_col and X_test_row: self.__call__(Y_train_col, X_test_row)
+        if isinstance(x, int) and isinstance(y, int): self.__call__(x, y)
 
 
-    def __call__(self, Y_train_col: Union[int, bool] = False, X_test_row: Union[int, bool] = False):
+    def __call__(self,  x: Union[int, bool] = False, y: Union[int, bool] = False):
         "Returns divided X_train, Y_train, X_test if division done."
 
         # if data for division given
-        if Y_train_col and X_test_row:
-            self.divide(Y_train_col, X_test_row)
+        if isinstance(x, int) and isinstance(y, int):
+            self.divide(x, y)
             return self.__X_train, self.__Y_train, self.__X_test
 
         # just returns existing data
@@ -39,42 +39,18 @@ class table_aggregation:
             return [False, message]
 
 
-    def divide(self, Y_train_col: int, X_test_row: int):
+    def divide(self,  x: int, y: int):
         "Gives X_train, Y_train, X_test."
 
-        # temporary variables
-        temp_rows = []
+        if y < 0 or y >= len(self.__row_table):
+            message = "Invalid row index"
+            print(message)
+            return [False, message]
+        if x < 0 or x >= len(self.__row_table[0]):
+            message = "Invalid column index"
+            print(message)
+            return [False, message]
 
-        # saving X_test
-        self.__X_test = self.__row_table[X_test_row]
-
-        # excluding X_test row
-        if   X_test_row!=0 and X_test_row!=len(self.__row_table):
-
-            temp_rows+=self.__row_table[:X_test_row]
-            temp_rows+=self.__row_table[X_test_row+1:]
-
-        elif X_test_row!=0: temp_rows = self.__row_table[:-1]
-        else: temp_rows = self.__row_table[1:]
-
-        # creating Y_train_col and excluding Y_train_col for X_train table
-        # reset
-        self.__X_train = []
-        self.__Y_train = []
-
-        for x in temp_rows:
-
-            # temporary row
-            temp_row = []
-
-            if   Y_train_col!=0 and Y_train_col!=len(x):
-
-                temp_row+=x[:Y_train_col]
-                temp_row+=x[Y_train_col+1:]
-
-            elif Y_train_col!=0: temp_row = x[:-1]
-            else: temp_row = x[1:]
-
-            # append to class lists
-            self.__X_train.append(temp_row)
-            self.__Y_train.append(x[Y_train_col])
+        self.__X_train  = [row[:x] + row[x+1:] for i, row in enumerate(self.__row_table) if i != y]
+        self.__Y_train  = [row[x] for i, row in enumerate(self.__row_table) if i != y]
+        self.__X_test   = self.__row_table[y][:x] + self.__row_table[y][x+1:]
